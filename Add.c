aca26853a023aa9bc557cb8f4115cef234431b87
mahendrakumar2000 <116829995+mahendrakumar2000@users.noreply.github.com>
2022-11-04 11:56:09 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,12 @@
+#include<stdoi.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    clrscr();
+    printf("enter a number\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("addition=%d",c);
+    getch();
+}

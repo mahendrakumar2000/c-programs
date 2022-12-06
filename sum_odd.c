343adb61cfadd7cb8335904bffc95db6f3361e10
mahendrakumar2000 <116829995+mahendrakumar2000@users.noreply.github.com>
2022-12-06 21:58:21 +0530

    Add files via upload

---
+++ sum_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,sum=0;
+    clrscr();
+    printf("enter a limit\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i+2)
+    sum=sum+i;
+    printf("sum of odd numbers=%d\n",sum);
+    getch();)
+}

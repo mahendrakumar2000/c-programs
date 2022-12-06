7eb01558015c9f83b58be83b7803d7dd9ae6cda2
mahendrakumar2000 <116829995+mahendrakumar2000@users.noreply.github.com>
2022-12-06 21:56:49 +0530

    Add files via upload

---
+++ biggest_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    clrscr();
+    printf("enter a,b value\n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("%d is big",a);
+    else
+    printf("%d is big",b);
+    getch()
+}

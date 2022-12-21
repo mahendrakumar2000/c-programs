6b39094287a861686c15baa8c098055ff97773fa
mahendrakumar2000 <116829995+mahendrakumar2000@users.noreply.github.com>
2022-12-21 22:10:54 +0530

    Add files via upload

---
+++ sign_unsign.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    short a=32767;
+    unsigned short b=65535;
+    clrscr();
+    printf("signed short %d\n",a);
+    printf("unsigned short%u\n",b);
+    getch();
+}

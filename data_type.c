2cece280d0fffb84a5738214519740f270cd7393
mahendrakumar2000 <116829995+mahendrakumar2000@users.noreply.github.com>
2022-12-21 22:10:26 +0530

    Add files via upload

---
+++ data_type.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5;
+    float b=15;
+    char ='b';
+    clrscr();
+    printf("integer value\n",a);
+    printf("float value\n",b);
+    printf("char value\n",c);
+    getch();
+}
